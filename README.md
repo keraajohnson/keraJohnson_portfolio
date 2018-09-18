527aa04c9577753abb506343baf27794d7102399
Kera <kera.andrea@gmail.com>
2018-09-18 12:24:11 -0400

    so much has changed, I can't even

--- README.md
+++ README.md
@@ -1,10 +1,4 @@
 # Static Boilerplate
 
-### Getting started
-
-Clone this repository
-In terminal, run `rm -r .git` to remove any commit history from the current repo
-
-Run `git init` to create a new `.git` folder
-
-Run `npm i` to get started, and run `gulp` to start the server!
+Hi! 
+I created this site with my own bare hands. 
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,26 @@
-const hello = 'World';
-console.log(hello);
+    // header
+    const typed = new Typed('.typedStrings', {
+        strings: ["the internet", "javascript", "design", "dogs", "reality TV", "running", "#a11y", "react"],
+        typeSpeed: 55,
+        backSpeed: 40,
+        showCursor: true,
+        loop: true
+    });
+    
+$(function () {
+    // flickity carousel
+    $('.mainCarousel').flickity({
+        // options
+        cellAlign: 'left',
+        contain: true,
+        cellSelector: '.project',
+        prevNextButtons: true,
+        pageDots: false,
+        wrapAround: true
+    });	
+    new WOW().init();
+
+    // smooth scroll
+    $('nav ul li a').smoothScroll();
+});
+
